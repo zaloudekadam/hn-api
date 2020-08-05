dc5af65997282031bcaca7b5d02285273465545b
Adam Zaloudek <mail@zaloudek.dev>
2020-08-05 21:44:23 +0200

    serverless

---
+++ api/getBest.js
@@ -0,0 +1,32 @@
+
+async function getBest() {
+  const response = await fetch("https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty");
+  const body = await response.json();
+
+  const resultPromises = body.map(async id => {
+    const itemResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+    const itemBody = await itemResponse.json();
+    if (itemBody.score > 100) {
+
+      return {
+        id: itemBody.id,
+        by: itemBody.by,
+        score: itemBody.score,
+        title: itemBody.title,
+        url: itemBody.url,
+        time: itemBody.time,
+        type: itemBody.type
+      }
+    }
+
+    return null;
+  });
+
+  const result = await Promise.all(resultPromises);
+
+  return result.filter(e => e != null);
+}
+
+export default (req, rese) => {
+  res.json(getBest());
+}
---
+++ api/getNew.js
@@ -0,0 +1,33 @@
+async function getNew(p = 50) {
+  const response = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty");
+  const body = await response.json();
+
+  const resultPromises = body.map(async id => {
+    const itemResponse = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
+    const itemBody = await itemResponse.json();
+    if (itemBody) {
+      if (itemBody.score >= p) {
+        //console.log('itemBody', itemBody);
+        return {
+          id: itemBody.id,
+          by: itemBody.by,
+          score: itemBody.score,
+          title: itemBody.title,
+          url: itemBody.url,
+          time: itemBody.time,
+          type: itemBody.type
+        }
+      }
+    }
+
+    return null;
+  });
+
+  const result = await Promise.all(resultPromises);
+
+  return result.filter(e => e != null);
+};
+
+export default (req, rese) => {
+  res.json(getNew());
+}
